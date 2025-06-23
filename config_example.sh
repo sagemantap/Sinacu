87405e54a3e786de3448bec2cbfb2683bcabcff6
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-06-23 15:48:09 +0700

    Update config_example.sh

--- config_example.sh
+++ config_example.sh
@@ -1,1 +1,1 @@
-./xlarig -o mine.scalaproject.io:3333 -u Ssy2L9U75D2JomhUToLSNQhybW4fDyJCVgaH95UTvRWNEX7UAYdpVrjfbqZuXqj4r3TgSnoiq7mDVRq72BmJtVP93TcAR9oeHJ -p rig_example
+./xlarig -o mine.scalaproject.io:3333 -u Ssy28g1vp2S2BrCPWWwcHESA84p287845SUZGnsL7qwn7v3PLAFVNfgiALU6ftStC3du4GDr7h484Y2R2YDkth2r7G9cpCVETH -p Danis
